911a7ca05656b44a5ce0f74deb444494aabdb4d6
Mukul Bedi <mukulbedi07@gmail.com>
2024-07-23 11:49:06 +0530

    Admin Roles and Add/edit products

--- src/features/admin/components/AdminProductList.js
+++ src/features/admin/components/AdminProductList.js
@@ -1,14 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { selectError, selectLoggedInUser } from '../authSlice';
+import { Link, Navigate } from 'react-router-dom';
+import { checkUserAsync } from '../authSlice';
 import { useForm } from 'react-hook-form';
 
-import { selectLoggedInUser, createUserAsync } from '../authSlice';
-import { Link } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
-
-export default function Signup() {
+export default function Login() {
   const dispatch = useDispatch();
+  const error = useSelector(selectError);
   const user = useSelector(selectLoggedInUser);
-
   const {
     register,
     handleSubmit,
@@ -28,26 +27,19 @@ export default function Signup() {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Create a New Account
+            Log in to your account
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form
             noValidate
-            className="space-y-6"
             onSubmit={handleSubmit((data) => {
               dispatch(
-                createUserAsync({
-                  email: data.email,
-                  password: data.password,
-                  addresses: [],
-                  role:'user'
-                  //TODO: this role can be directly given on backend
-                })
+                checkUserAsync({ email: data.email, password: data.password })
               );
-              console.log(data);
             })}
+            className="space-y-6"
           >
             <div>
               <label
@@ -83,19 +75,20 @@ export default function Signup() {
                 >
                   Password
                 </label>
+                <div className="text-sm">
+                  <Link
+                    to="/forgot-password"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </Link>
+                </div>
               </div>
               <div className="mt-2">
                 <input
                   id="password"
                   {...register('password', {
                     required: 'password is required',
-                    pattern: {
-                      value:
-                        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,
-                      message: `- at least 8 characters\n
-                      - must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number\n
-                      - Can contain special characters`,
-                    },
                   })}
                   type="password"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -104,34 +97,7 @@ export default function Signup() {
                   <p className="text-red-500">{errors.password.message}</p>
                 )}
               </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Confirm Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="confirmPassword"
-                  {...register('confirmPassword', {
-                    required: 'confirm password is required',
-                    validate: (value, formValues) =>
-                      value === formValues.password || 'password not matching',
-                  })}
-                  type="password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-                {errors.confirmPassword && (
-                  <p className="text-red-500">
-                    {errors.confirmPassword.message}
-                  </p>
-                )}
-              </div>
+              {error && <p className="text-red-500">{error.message}</p>}
             </div>
 
             <div>
@@ -139,18 +105,18 @@ export default function Signup() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign Up
+                Log in
               </button>
             </div>
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Already a Member?{' '}
+            Not a member?{' '}
             <Link
-              to="/login"
+              to="/signup"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
-              Log In
+              Create an Account
             </Link>
           </p>
         </div>
